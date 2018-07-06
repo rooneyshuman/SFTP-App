5afa7c38f30d28ca38ab8f00cdbce55c2e33fdef
Brent McManus <brent.mcmanus@me.com>
2018-07-05 17:08:35 -0700

    added sample test framework

--- Main/src/SFTP.java
+++ Main/src/SFTP.java
@@ -4,5 +4,7 @@ import com.jcraft.jsch.*;
  * Class definition of sftp client class
  */
 public class SFTP {
+    public static void main(String[] args) {
 
+    }
 }
--- Main/src/SFTPtest.java
+++ Main/src/SFTPtest.java
@@ -2,6 +2,12 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+/**
+ * Class for testing SFTP method functionality
+ */
 public class SFTPtest {
+    @Test
+    public void thisIsWhatUnitTestStructureLooksLike() {
 
+    }
 }
