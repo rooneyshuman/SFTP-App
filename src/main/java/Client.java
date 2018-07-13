06ecb48843d1bd0e7463e1fc69e652cab0b9b644
Brent McManus <brent.mcmanus@me.com>
2018-07-13 14:10:24 -0700

    Added loops to menus

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,82 +6,85 @@ class Client {
   void Sftp() {
     var jsch = new JSch();
     int option;
-
     var menu = new Menu();
-    if (menu.mainMenu() == 2) {
-      System.out.println("Goodbye");
-      System.exit(0);
-    } else {
-      try {
-        var user = new User();
-        String username = user.getUsername();
-        String password = user.getPassword();
-        String hostname = user.getHostname();
-        Session session = jsch.getSession(username, hostname, 22);
-        session.setPassword(password);
-        session.setConfig("StrictHostKeyChecking", "no");
-        System.out.println("Establishing Connection...");
-        session.connect();
 
-        Channel channel = session.openChannel("sftp");
-        channel.connect();
-        ChannelSftp cSftp = (ChannelSftp) channel;
+    do{
+      option = menu.mainMenu();
+      if (option ==1) {
+        try {
+          var user = new User();
+          String username = user.getUsername();
+          String password = user.getPassword();
+          String hostname = user.getHostname();
+          Session session = jsch.getSession(username, hostname, 22);
+          session.setPassword(password);
+          session.setConfig("StrictHostKeyChecking", "no");
+          System.out.println("Establishing Connection...");
+          session.connect();
 
-        System.out.println("Successful SFTP connection");
+          Channel channel = session.openChannel("sftp");
+          channel.connect();
+          ChannelSftp cSftp = (ChannelSftp) channel;
 
-        do {
-          option = menu.workingMenu();
-          switch (option) {
-            case 1: //list directories: local and remote options
-              System.out.println("Listing directories...");
-              break;
+          System.out.println("Successful SFTP connection");
 
-            case 2: //get file/files: which files, put where
-              System.out.println("Getting files...");
-              break;
+          do {
+            option = menu.workingMenu();
+            switch (option) {
+              case 1: //list directories: local and remote options
+                System.out.println("Listing directories...");
+                break;
 
-            case 3: //put file/files: which files put where
-              System.out.println("Putting Files...");
-              break;
+              case 2: //get file/files: which files, put where
+                System.out.println("Getting files...");
+                break;
 
-            case 4: //create directory: name?, where?
-              System.out.println("Creating directories...");
-              break;
+              case 3: //put file/files: which files put where
+                System.out.println("Putting Files...");
+                break;
 
-            case 5: //delete file/directory
-              System.out.println("Deleting directories...");
-              break;
+              case 4: //create directory: name?, where?
+                System.out.println("Creating directories...");
+                break;
 
-            case 6: //change permissions
-              System.out.println("Changing permissions...");
-              break;
+              case 5: //delete file/directory
+                System.out.println("Deleting directories...");
+                break;
 
-            case 7: //copy directory
-              System.out.println("Copying directories...");
-              break;
+              case 6: //change permissions
+                System.out.println("Changing permissions...");
+                break;
 
-            case 8: //rename file
-              System.out.println("Renaming files...");
-              break;
+              case 7: //copy directory
+                System.out.println("Copying directories...");
+                break;
 
-            case 9: //view log history
-              System.out.println("Viewing log history...");
-              break;
+              case 8: //rename file
+                System.out.println("Renaming files...");
+                break;
 
-            case 10: //exit
-              System.out.println("Closing connection...");
-              cSftp.exit();
-              break;
+              case 9: //view log history
+                System.out.println("Viewing log history...");
+                break;
 
-            default:
-              System.out.println("Try again");
-              break;
-          }
-        } while (option != 10);
-      } catch (Exception e) {
-        System.out.println("Client error");
+              case 10: //exit
+                System.out.println("Closing connection...");
+                cSftp.exit();
+                break;
+
+              default:
+                System.out.println("Try again");
+                break;
+            }
+          } while (option != 10);
+        } catch (Exception e) {
+          System.err.println("Client error");
+          e.printStackTrace();
+          System.exit(1);
+        }
       }
-    }
+    } while (option != 2);
+    System.out.println("Goodbye");
     System.exit(0);
   }
 }
