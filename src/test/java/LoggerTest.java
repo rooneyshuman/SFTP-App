456a5081544206b90496e491f5fc67ab6d9aecf3
Brent McManus <brent.mcmanus@me.com>
2018-08-09 16:52:38 -0700

    error with save test

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -1,31 +1,42 @@
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class LoggerTest {
   private String test = "Test";
+  Logger log = new Logger();
 
   @Test
   public void logPrintsCorrectly() {
-    var log = new Logger();
     assertThat(log.logHistory.size(), equalTo(0));
     log.log(test);
     assertThat(log.logHistory.size(), equalTo(1));
   }
 
   @Test
-  public void display() {
+  public void displaySingleLogCorrectly() {
     OutputStream os = new ByteArrayOutputStream();
     PrintStream ps = new PrintStream(os);
     var log = new Logger(ps);
     log.log(test);
     log.display();
-    assertThat(new SimpleDateFormat("MM/dd/yy hh:mm a").format(log.timestamp) + ": " + test + "\n",equalTo(os.toString()));
+    assertThat(new SimpleDateFormat("MM/dd/yy HH:mm:ss").format(log.timestamp) + ": " + test + "\n",equalTo(os.toString()));
+  }
+
+  @Test
+  public void saveCreatesCorrectFile() {
+    log = new Logger();
+    log.log(test);
+    var timestamp = new Date(System.currentTimeMillis());
+    String home = System.getProperty("user.home");
+    String filename = "SFTP Log History - " + test + " " + new SimpleDateFormat("MM/dd/yy HH:mm").format(timestamp) + ".txt";
+    File file = new File(home + "/Downloads/" + filename);
+    log.save(test);
+    assertThat(file.exists(), equalTo(true));
   }
 }
