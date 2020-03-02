66e3cd54cfcb797c106dae24471f4253335edc71
Carissa Allen <carissawood@outlook.com>
2020-03-02 14:28:25 -0800

    Delete redundant "return" in Javadoc

--- src/main/java/edu/pdx/cs/sftp/Client.java
+++ src/main/java/edu/pdx/cs/sftp/Client.java
@@ -144,12 +144,12 @@ public class Client {
     out.println("A log file has been saved to your local Downloads directory");
   }
 
-  /** @return returns a Channel object connected to an SFTP server. */
+  /** @return a Channel object connected to an SFTP server. */
   ChannelSftp getChannelSftp() {
     return channelSftp;
   }
 
-  /** @return returns a Session object representing a connection to an SSH server. */
+  /** @return a Session object representing a connection to an SSH server. */
   Session getSession() {
     return session;
   }
