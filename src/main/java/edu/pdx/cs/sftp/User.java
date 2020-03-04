c1eb5f1a3e0a92e6e0a217ddc3256b33076fbeb8
Bassel Hamieh <bahamieh@pdx.edu>
2020-03-04 14:45:28 -0800

    Incorporate team reviews from previous pull request
    Takes review change suggestions from previous pull request and implements them.

--- src/main/java/edu/pdx/cs/sftp/User.java
+++ src/main/java/edu/pdx/cs/sftp/User.java
@@ -9,8 +9,8 @@ import static java.lang.System.out;
 import static java.lang.System.in;
 
 /**
- * Gathers information about a connection attempt initiated by a user, verifies
- * user input against a set of input rules, and returns valid credentials.
+ * Gathers information about a connection attempt initiated by a user, verifies user input against a
+ * set of input rules, and returns valid credentials.
  */
 class User {
   String username;
@@ -24,7 +24,7 @@ class User {
     password = null;
     hostname = null;
   }
-  
+
   /**
    * Class constructor setting all fields to the current input.
    *
@@ -51,54 +51,50 @@ class User {
   }
 
   /**
-   * Prompt the user for a valid password.
-   * Password must not be empty or include spaces.
+   * Prompts the user for a valid password. Password must not be empty or include spaces.
    *
-   * @return password input.
+   * @return validated password.
    */
   String getPassword() {
     out.println("Enter your password:");
     password = scanner.next();
     while (password == null || password.isEmpty() || !verifyPassword(password)) {
-      err.println("You did not enter a password.\n"
-                + "Please, enter your password:");
+      err.println("You did not enter a password.\n" + "Please, enter your password:");
       password = scanner.next();
     }
     return password;
   }
 
   /**
-   * Verify the password isn't empty and doesn't contain spaces.
+   * Verifies the password isn't empty and doesn't contain spaces.
    *
    * @param passwordToVerify is the string to be assessed.
-   * @return <code>true</code> if the string is a valid password <code>false</code> otherwise.
+   * @return <code>true</code> if the password entered is valid; <code>false</code> otherwise.
    */
   private boolean verifyPassword(String passwordToVerify) {
     return !passwordToVerify.isEmpty() && !passwordToVerify.contains(" ");
   }
 
   /**
-   * Prompt the user for a valid username.
-   * Username must be alphanumeric of size 8-20.
+   * Prompts the user for a valid username. Username must be alphanumeric of size 8-20.
    *
-   * @return username input.
+   * @return validated username.
    */
   String getUsername() {
     out.println("Enter your username:");
     username = scanner.next();
     while (username == null || username.isEmpty() || !verifyUsername(username)) {
-      err.println( "The username is invalid.\n"
-                 + "Please enter 8-20 alphanumeric characters.");
+      err.println("The username is invalid.\n" + "Please enter 8-20 alphanumeric characters.");
       username = scanner.next();
     }
     return username;
   }
 
   /**
-   * Verify the username is 8-20 alphanumeric characters.
+   * Verifies the username is 8-20 alphanumeric characters.
    *
    * @param usernameToVerify is the string to be checked against a regular expression.
-   * @return <code>true</code> if the string is a valid username <code>false</code> otherwise.
+   * @return <code>true</code> if the username entered is valid; <code>false</code> otherwise.
    */
   boolean verifyUsername(String usernameToVerify) {
     String usernamePattern = "^(?=.{2,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$";
@@ -108,34 +104,33 @@ class User {
   }
 
   /**
-   * Prompt the user for a valid host name, which follows the below parameters:
-   * (1) Must be alphanumeric
-   * (2) Can't be longer than 255 characters
-   * (3) Host name segments cannot exceed 63 characters
-   * (4) Must end and start with alphanumeric characters
+   * Prompts the user for a valid host name, which follows the below parameters: (1) Must be
+   * alphanumeric (2) Can't be longer than 255 characters (3) Host name segments cannot exceed 63
+   * characters (4) Must start and end with alphanumeric characters
    *
-   * @return host name input.
+   * @return validated hostname.
    */
   String getHostname() {
     out.println("Enter your hostname:");
     hostname = scanner.next();
-    while (hostname == null || hostname.isEmpty() || !verifyHostName(hostname)) {
-      err.println("The host name is invalid.\n"
-                + "Valid host names are no longer than 255 alphanumeric characters and dashes.\n"
-                + "Each segment of the host name cannot be longer than 63 characters.");
+    while (hostname == null || hostname.isEmpty() || !verifyHostname(hostname)) {
+      err.println(
+          "The hostname is invalid.\n"
+              + "Valid hostnames are no longer than 255 alphanumeric characters and dashes.\n"
+              + "Each segment of the hostname cannot be longer than 63 characters.");
       hostname = scanner.next();
     }
     return hostname;
   }
 
   /**
-   * Verify the host name is alphanumeric, shorter than 255 characters, with name segments not
+   * Verifies the host name is alphanumeric, shorter than 255 characters, with name segments not
    * exceeding 63 characters, and starts and ends with alphanumeric characters.
    *
    * @param hostnameToVerify is a string to be checked against a regular expression.
-   * @return <code>true</code> if the string is a valid host name <code>false</code> otherwise.
+   * @return <code>true</code> if the hostname entered is valid; <code>false</code> otherwise.
    */
-  boolean verifyHostName(String hostnameToVerify) {
+  boolean verifyHostname(String hostnameToVerify) {
     if (hostnameToVerify.length() > 255) return false;
     String hostnamePattern =
         "^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])(\\."
